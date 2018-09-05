20c7f9548ee4a6b7d5d42ed4a9fa7f998598429c
Ashley Muller <serenehopes@gmail.com>
2018-09-05 06:00:38 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,8 +5,8 @@ def fizzbuzz(num)
   if num % 3 && num % 5 == 0
     return "FizzBuzz"
   elsif num % 5 == 0
-    return "Fizz"
-  elsif num % 3 == 0
     return "Buzz"
+  elsif num % 3 == 0
+    return "Fizz"
   end
 end
